3d9e0456e0ab222f119243d335103025412f56f5
Sailesh <shailesh.mrzn@gmail.com>
2024-07-10 22:58:42 +0545

    completed backend and frontend init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,76 @@
-function App() {
+import { useState } from "react";
+import { itemsList } from "../itemsList.ts";
+import axios from "axios";
+
+const App: React.FC = () => {
+  const [selectedItems, setSelectedItems] = useState<String[]>([]);
+  const [packages, setPackages] = useState<any[]>([]);
+
+  const handleItemSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const itemName = e.target.value;
+    if (e.target.checked) {
+      setSelectedItems([...selectedItems, itemName]);
+    } else {
+      setSelectedItems(selectedItems.filter((item) => item !== itemName));
+    }
+  };
+
+  console.log("selecteditems", selectedItems);
+
+  const handlePlaceOrder = async () => {
+    try {
+      const response = await axios.post("http://localhost:5555/place-order", {
+        selectedItems,
+      });
+      setPackages((prevPackages) => [...prevPackages, ...response.data]);
+      console.log(response.data);
+      setSelectedItems([]); // Clear the selection
+      console.log("posted");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+    console.log("packages", packages);
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <h1>Place Your Order</h1>
+      <ul>
+        {itemsList.map((item) => (
+          <li key={item.name}>
+            <label>
+              <input
+                type="checkbox"
+                value={item.name}
+                onChange={handleItemSelection}
+                checked={selectedItems.includes(item.name)}
+              />
+              {item.name} - ${item.price} - {item.weight}g
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handlePlaceOrder}>Place Order</button>
+      <h2>Packages</h2>
+      {packages.length === 0 ? (
+        <p> No any packages</p>
+      ) : (
+        packages.map((pkg, index) =>
+          pkg.totalPrice > 250 ? (
+            <div>Total price in a package cannot exceed $250</div>
+          ) : (
+            <div key={index}>
+              <h3>Package {index + 1}</h3>
+              <p>Items: {pkg.items.join(", ")}</p>
+              <p>Total weight: {pkg.totalWeight}g</p>
+              <p>Total price: ${pkg.totalPrice}</p>
+              <p>Courier price: ${pkg.courierPrice}</p>
+            </div>
+          )
+        )
+      )}
+    </div>
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,66 @@
 import express from "express";
 import dotenv from "dotenv";
+import { itemsList, courierCharges } from "./itemsList.js";
+import bodyparser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(bodyparser.json());
+app.use(cors());
 
 const port = process.env.PORT;
+
+function calculateCourierCharge(weight) {
+  for (let i = 0; i < courierCharges.length; i++) {
+    if (weight <= courierCharges[i].maxWeight) {
+      return courierCharges[i].charge;
+    }
+  }
+  return 0;
+}
+
+const createPackage = (selectedItems) => {
+  const finalPackage = [];
+  let currentPackage = {
+    items: [],
+    totalWeight: 0,
+    totalPrice: 0,
+  };
+
+  for (let item of selectedItems) {
+    const itemDetails = itemsList.find((i) => i.name === item);
+
+    //check if the price limit exceeds
+    if (currentPackage.totalPrice + itemDetails.price > 250) {
+      console.log("error");
+
+      console.log(finalPackage);
+    }
+
+    currentPackage.items.push(itemDetails.name);
+    currentPackage.totalWeight += itemDetails.weight;
+    currentPackage.totalPrice += itemDetails.price;
+  }
+
+  finalPackage.push({
+    ...currentPackage,
+    courierPrice: calculateCourierCharge(currentPackage.totalWeight),
+  });
+
+  // for (let pkg of finalPackage) {
+  //   calculateCourierCharge(pkg.totalWeight);
+  // }
+
+  return finalPackage;
+};
+
+app.post("/place-order", (req, res) => {
+  const { selectedItems } = req.body;
+  const order = createPackage(selectedItems);
+  res.json(order);
+});
 
 app.listen(port, () => {
   console.log(`App is listening to port ${port}`);
